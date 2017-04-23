0fef162d26733a5be6313f9fe0ded57315e6a746
jchin <justinchin.bcchs@gmail.com>
2017-04-22 22:14:09 -0600

    Dragable sockets

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
+
+const uuidV4 = require('uuid/v4');
+
 //var coordinates = require('/public/canvas.js');
 
 app.use(express.static("public"));
@@ -13,19 +16,20 @@ app.get("/", function(req,res){
 	res.render("canvas");
 });
 
-//app.listen(4000, 'localhost', function()
-//{
-//	console.log("Server is listening");
-//});
 
-
-//Client Array
+//Client Arrays
 var clients = [];
+var webclients =[];
+
+var NEWCOORD; 
+
 
 //socket is the connected client 
 io.on("connection", function(socket){
 	
 	var currentUser;
+
+
 
 	//Send connected user info to socket client
 	socket.on("USER_CONNECT", function(){
@@ -43,25 +47,50 @@ io.on("connection", function(socket){
 	socket.on("PLAY", function(data){
 		console.log(data);
 		currentUser = {
-			name:data.name,
+			name:"UNITYCLIENT_"+uuidV4(),
 			position:data.position
 		}
 
 		clients.push(currentUser);
+		console.log(currentUser.name);
 		socket.emit("PLAY", currentUser)
 
 		//Broadcast to all
 		socket.broadcast.emit("USER_CONNECTED", currentUser)
 	});
 
-	//CONNECTION FROM WEBSITE
-	socket.on("WEB_CONNECT", function(data){
-		console.log(data);
-	});
+
 
 	//CONNECTION FROM WEBSITE
-	socket.on("COORD", function(data){
-		console.log(data);
+	socket.on("WEBCLIENT_CONNECT", function(data){
+		
+		currentUser = {
+			name:"WEBCLIENT_"+uuidV4(),
+		}
+		webclients.push(currentUser);
+
+		console.log(currentUser.name);
+
+		//Broadcast to all
+		console.log("Web user connected: " + currentUser.name);
+		socket.broadcast.emit("USER_CONNECTED", currentUser)
+	});
+
+
+
+
+	//New Coord Update FROM webclient, and broadcast to all
+	socket.on("NEW COORD", function(newCoord){
+		var NEWCOORD2;
+		NEWCOORD2 = newCoord;
+
+		NEWCOORD2 = {
+			coordval:newCoord
+		}
+		console.log(NEWCOORD2)
+
+		socket.broadcast.emit("boop");
+		socket.broadcast.emit("GET_NEW_COORD", NEWCOORD2)
 	});
 
 	//1.  when client sends MOVE event
@@ -75,12 +104,31 @@ io.on("connection", function(socket){
 		socket.broadcast.emit("MOVE", currentUser)
 	});
 
+	socket.on("PLAYER_POS", function(data){
+
+		var playerPos =
+		{
+			x:data.posx,
+			y:data.posy
+		}
+		//console.log(currentUser.name+"move to "+currentUser.position)
+		//Broadcast to all
+		socket.broadcast.emit("WEB_UPDATE_PLAYER", playerPos)
+	});
+
+
 	socket.on("disconnect", function(){
 		socket.broadcast.emit("USER_DISCONNECTED", currentUser)
 		for (var i=0; i < clients.length; i++ ){
 			if(clients[i].name == currentUser.name){
 				console.log("User "+clients[i].name+" disconnected");
 				clients.splice(i,1);
+			}
+		}
+		for (var i=0; i < webclients.length; i++ ){
+			if(webclients[i].name == currentUser.name){
+				console.log("WebClient "+webclients[i].name+" disconnected");
+				webclients.splice(i,1);
 			}
 		}
 	});
--- public/canvas.js
+++ public/canvas.js
@@ -1,25 +1,55 @@
-var elem = document.getElementById('myCanvas'),
-    elemLeft = elem.offsetLeft,
-    elemTop = elem.offsetTop,
-    context = elem.getContext('2d'),
+var pos;
+var dragged;
+
+var prev_player_posX;
+var prev_player_posY;
+
+
+var canvas = document.getElementById('myCanvas'),
+    elemLeft = canvas.offsetLeft,
+    elemTop = canvas.offsetTop,
+    context = canvas.getContext('2d'),
     elements = [];
     coords = "";
 
-// Add event listener for `click` events.
-elem.addEventListener('click', function(event) {
-    var x = event.pageX - elemLeft,
-        y = event.pageY - elemTop;
-    console.log(x, y);
+function allowDrop(ev) {
+    ev.preventDefault();
+}
 
-     socket.emit('COORD', [x,y]);
+// function get_pos(ev){
+//     dragPos = [ev.pageX, ev.pageY];
+//     console.log(dragPos);
+// }
 
-    coords = x + " " + y;
-    elements.forEach(function(element) {
-        if (y > element.top && y < element.top + element.height && x > element.left && x < element.left + element.width) {
-            alert('clicked an element');
-        }
-    });
+function drag(ev) {
+    ev.dataTransfer.setData("Text",ev.target.id);
+}
 
-}, false);
+function movePlayer(position){
+    //Clear previoius pos
+    document.getElementById("myCanvas").getContext("2d").clearRect(prev_player_posX,prev_player_posY,32,32);
 
-//module.exports{coords}
+    document.getElementById("myCanvas").getContext("2d").drawImage(document.getElementById("player"), position.x, position.y, 32, 32);
+    prev_player_posY = position.y;
+    prev_player_posX = position.x;
+}
+
+function drop(ev) {
+    var dropPos = [ev.pageX - elemLeft, ev.pageY - elemTop];
+    context.font='36px FontAwesome';
+    ev.preventDefault();
+    var offset = ev.dataTransfer.getData("text/plain").split(',');
+    var data=ev.dataTransfer.getData("Text");
+
+    var img = canvas = document.getElementById(data);
+    var dx = dropPos[0] - img.width / 2;
+    var dy = dropPos[1] - img.height / 2;
+
+    socket.emit('NEW COORD', [dropPos[0],dropPos[1]] );
+
+    document.getElementById("myCanvas").getContext("2d").drawImage(document.getElementById(data), dx, dy, 32, 32);
+    elements.push(dropPos);
+    // elements[elements.length - 1].width /= 2;
+    // elements[elements.length - 1].height /= 2;
+    console.log(dropPos[0] + " " + dropPos[1] + " " + elements.length);
+}
